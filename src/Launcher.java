4ec90a1c77e5a75d89f95c38aa6d87550ab3ed88
Hector6704 <hectordeponcins@gmail.com>
2022-09-20 15:18:11 +0200

    Part 4

--- src/Launcher.java
+++ src/Launcher.java
@@ -5,9 +5,23 @@ public class Launcher {
         System.out.println("Hello World!");
         var scanner = new Scanner(System.in);
         var input = scanner.nextLine();
-        while (!"quit".equals(input)) {
+        while (!"quit".equals(input) && !"fibo".equals(input)) {
             System.out.println("Unknown command");
             input = scanner.nextLine();
         }
+        if ("fibo".equals(input)) {
+            System.out.println("Enter a number");
+            var number = scanner.nextInt();
+            System.out.println(fibonacci(number));
+        }
+    }
+    public  static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
